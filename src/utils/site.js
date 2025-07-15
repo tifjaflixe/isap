5a62ca3be8a5a1a638ec308b5a4bea79b9c7f418
tifjaflixe <tifjaflixe@gmail.com>
2025-07-15 14:54:36 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Isap';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Amoy Viral Colmek Sma Cindo Tante China';
+export const url = 'https://isap.pages.dev';
+export const terbit = '2025-01-27T00:01:03Z';
+
+export const google = '2t_ZTB7zrtg4RbsbzNrhPKoYhgO1SNviBNhXc-CHwX0';
+export const yandex = '03c8bb52b11d1c56'
+
+export const index = '8e20152a-e530-4b30-8656-244a62dc352c';
